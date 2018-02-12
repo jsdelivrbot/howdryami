217172c242ab9bc332e16f7fa1575f93078f8e67
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-12 20:02:11 +0100

    Added dockerstart script.

---
+++ dockerstart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "stopping running application"
+ssh root@46.101.175.155 'docker stop howdryami'
+ssh root@46.101.175.155 'docker rm howdryami'
+
+echo "pulling latest version of the code"
+ssh root@46.101.175.155 'docker pull terjeofnorway/howdryami:latest'
+
+echo "starting the new version"
+ssh root@46.101.175.155 'docker run -d --restart=always --name dodsv -p 80:5000 terjeofnorway/howdryami:latest'
+
+echo "success!"
+
+exit 0
