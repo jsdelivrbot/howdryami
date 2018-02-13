ee6eccaa145b86d78f823505866d36d49c803865
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-13 21:17:59 +0100

    Update dockerstart.sh

--- dockerstart.sh
+++ dockerstart.sh
@@ -10,10 +10,12 @@ echo ssh root@howdryami.deploythenscotch.com 'docker stop howdryami'
 ssh root@howdryami.deploythenscotch.com 'docker rm howdryami'
 
 echo "pulling latest version of the code"
-ssh root@howdryami.deploythenscotch.com 'docker pull terjeofnorway/howdryami:latest'
+ssh root@howdryami.deploythenscotch.com 'docker pull terjeofnorway/howdryami:master'
+ssh root@howdryami.deploythenscotch.com 'docker stop howdryami_master'
+ssh root@howdryami.deploythenscotch.com 'docker rm howdryami_master'
 
 echo "starting the new version"
-ssh root@howdryami.deploythenscotch.com 'docker run -d --restart=always --name dodsv -p 80:5000 terjeofnorway/howdryami:latest'
+ssh root@howdryami.deploythenscotch.com 'docker run -d --restart=always --name howdryami_master -p 80:5000 terjeofnorway/howdryami:master'
 
 echo "success!"
 
