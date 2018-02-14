cfc22eccd404e0bbeb2eb0c8f9b696463e3a68d0
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-14 09:07:57 +0100

    Update dockerstart.sh

--- dockerstart.sh
+++ dockerstart.sh
@@ -6,16 +6,16 @@ StrictHostKeyChecking no
 EOF
   
 echo "stopping running application"
-echo ssh -i root@howdryami.deploythenscotch.com 'docker stop howdryami'
-ssh root@howdryami.deploythenscotch.com 'docker rm howdryami'
+echo ssh -i whiskey@howdryami.deploythenscotch.com 'docker stop howdryami'
+ssh whiskey@howdryami.deploythenscotch.com 'docker rm howdryami'
 
 echo "pulling latest version of the code"
-ssh -i $PATH_TO_PRIVATE_KEY root@howdryami.deploythenscotch.com 'docker pull terjeofnorway/howdryami:master'
-ssh -i $PATH_TO_PRIVATE_KEY root@howdryami.deploythenscotch.com 'docker stop howdryami_master'
-ssh -i $PATH_TO_PRIVATE_KEY root@howdryami.deploythenscotch.com 'docker rm howdryami_master'
+ssh -i $PATH_TO_PRIVATE_KEY whiskey@howdryami.deploythenscotch.com 'docker pull terjeofnorway/howdryami:master'
+ssh -i $PATH_TO_PRIVATE_KEY whiskey@howdryami.deploythenscotch.com 'docker stop howdryami_master'
+ssh -i $PATH_TO_PRIVATE_KEY whiskey@howdryami.deploythenscotch.com 'docker rm howdryami_master'
 
 echo "starting the new version"
-ssh -i $PATH_TO_PRIVATE_KEY root@howdryami.deploythenscotch.com 'docker run -d --restart=always --name howdryami_master -p 80:5000 terjeofnorway/howdryami:master'
+ssh -i $PATH_TO_PRIVATE_KEY whiskey@howdryami.deploythenscotch.com 'docker run -d --restart=always --name howdryami_master -p 80:5000 terjeofnorway/howdryami:master'
 
 echo "success!"
 
