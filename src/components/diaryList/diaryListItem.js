778e3e8640c1c628e6b0e4cb91deba1be9733ed3
Terje Karlsen <terjeofnorway@gmail.com>
2018-02-24 17:58:38 +0100

    Added toggle to diaryListItem.

--- src/components/diaryList/diaryListItem.js
+++ src/components/diaryList/diaryListItem.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import classname from 'classname';
 
 import { View, Icon } from '../../particles';
 import Button from '../button/button';
@@ -10,21 +11,39 @@ import * as Icons from '../icons';
 
 import './diaryListItem.css';
 
-const DiaryListItem = props => {
-  const { label, time, icon } = props.diaryItem;
-  const { editClickHandler, deleteClickHandler } = props;
+class DiaryListItem extends Component {
+  state = {
+    editDeleteIsVisible: false,
+  }
 
-  return (
-    <View className="diarylist__item">
-      <Icon className="item__icon" image={Icons.getIconById(icon)} />
-      <View className="item__label">{label}</View>
-      <time className="item__time">{moment(time).format('HH:mm')}</time>
-      <View className="editdelete__container">
-        <Button className="item__edit" onClick={editClickHandler} type={Button.REGULAR} />
-        <Button className="item__delete" onClick={deleteClickHandler} type={Button.REGULAR} />
+  toggleEditdeletevisibility = () => {
+    console.log('toggle')
+    this.setState({ editDeleteIsVisible: !this.state.editDeleteIsVisible });
+  }
+
+  render() {
+    const { label, time, icon } = this.props.diaryItem;
+    const { toggleEditdeleteVisibility } = this;
+    const { editClickHandler, deleteClickHandler } = this.props;
+    const { editDeleteIsVisible } = this.state;
+
+    const editdeleteVisibilityStyleClasses = classname({
+      editdelete__container: true,
+      'editdelete__container--visible': editDeleteIsVisible,
+    });
+
+    return (
+      <View className="diarylist__item" onClick={this.toggleEditdeletevisibility}>
+        <Icon className="item__icon" image={Icons.getIconById(icon)} />
+        <View className="item__label">{label}</View>
+        <time className="item__time">{moment(time).format('HH:mm')}</time>
+        <View className={editdeleteVisibilityStyleClasses}>
+          <Button className="item__edit" onClick={editClickHandler} type={Button.REGULAR} />
+          <Button className="item__delete" onClick={deleteClickHandler} type={Button.REGULAR} />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 };
 
 DiaryListItem.propTypes = {
--- src/particles/view.js
+++ src/particles/view.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PT from 'prop-types';
 
 const View = props => (
-  <div className={props.className}>{props.children}</div>
+  <div className={props.className} onClick={props.onClick}>{props.children}</div>
 );
 
 View.propTypes = {
   children: PT.node,
   className: PT.string,
+  onClick: PT.func,
 };
 
 View.defaultProps = {
   children: '',
   className: '',
+  onClick: () => {},
 };
 
 export default View;
