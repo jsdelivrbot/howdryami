f99c7843878ab8219a1b47e924c14e23f2054ab5
Terje Karlsen <terjeofnorway@gmail.com>
2018-03-04 20:21:36 +0100

    Added routing to correct view after loading has finished.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+import PT from 'prop-types';
 
 import StartupLoader from './startupLoader/startupLoader';
 import Welcome from './welcome/welcome';
@@ -8,17 +10,43 @@ import Home from './home/home';
 import DiaryEntry from './diaryEntry/diaryEntry';
 import Confirm from '../components/confirm/confirm';
 
+import { loadUIData } from '../startup';
+
+import { userType } from '../ducks/user';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Confirm />
-    <Route exact path="/" component={StartupLoader} />
-    <Route exact path="/welcome" component={Welcome} />
-    <Route exact path="/register" component={Register} />
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/diaryentry" component={DiaryEntry} />
-  </div>
-);
+class App extends Component {
+  componentWillMount() {
+    const { dispatch, history } = this.props;
+    loadUIData(dispatch).then(result => {
+      const nextRoute = result.includes(userType.USER_HYDRATION_EMPTY) ? '/register' : '/home';
+      history.push(nextRoute);
+    });
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Confirm />
+        <Route exact path="/" component={StartupLoader} />
+        <Route exact path="/welcome" component={Welcome} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/diaryentry" component={DiaryEntry} />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  dispatch: PT.func.isRequired,
+  history: PT.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  dispatch: state,
+});
+
+
+export default withRouter(connect()(App));
--- src/startup.js
+++ src/startup.js
@@ -1,18 +1,18 @@
+import createHistory from 'history/createBrowserHistory';
 import uiData from './data/ui';
 import barData from './data/bar';
 import { barOperations } from './ducks/bar';
 import { uiOperations } from './ducks/ui';
-import { userOperations } from './ducks/user';
+import { userOperations, userType } from './ducks/user';
 import { diaryOperations } from './ducks/diary';
 
-export const loadUIData = store => {
-  const { dispatch } = store;
-  Promise.all([
+export const loadUIData = dispatch => {
+  const history = createHistory();
+
+  return Promise.all([
     dispatch(uiOperations.loadUIData(uiData)),
     dispatch(barOperations.loadBarData(barData)),
     dispatch(userOperations.hydrateUser()),
     dispatch(diaryOperations.hydrateDiary()),
-  ]).then(allReturnStatuses => {
-    console.log(allReturnStatuses);
-  });
+  ]);
 };
