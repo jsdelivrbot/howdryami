568881e01a57137c3fd6a3cc9ff41d6151cfeb2f
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-02 13:04:52 +0200

    Added support for environment variables. Redirect if not ssl.

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const publicDir = path.join(__dirname, 'build');
+const isRunningLocally = process.env.NODE_ENV === 'development';
 
 const PORT = process.env.PORT || 5000;
 
 express()
+  .use((req, res) => {
+    if (!req.secure && !isRunningLocally) {
+      const secureUrl = `https://${req.headers.host}${req.url}`;
+      res.writeHead(301, { Location: secureUrl });
+      res.end();
+    }
+  })
   .use(express.static(publicDir))
-  .get('/', (req, res) => res.sendFile('/index.html'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
