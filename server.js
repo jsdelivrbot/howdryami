2a93a48863c48ea00de5979347eb567f3083c0b4
Terje Karlsen <terjeofnorway@gmail.com>
2018-09-02 12:09:11 +0200

    Prep for Heroku: - Added Heroku-related rigging. - Removed Dockerfile.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const publicDir = path.join(__dirname, 'build');
+
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(publicDir))
+  .get('/', (req, res) => res.sendFile('/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
